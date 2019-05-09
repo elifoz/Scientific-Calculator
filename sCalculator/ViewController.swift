27f21f19a64945d48934ed8f22ddbefc5d228627
Mac14 <mac14@Mac14s-iMac.local>
2019-05-09 12:18:53 +0300

    Initial Commit

---
+++ sCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sCalculator
+//
+//  Created by Mac14 on 9.05.2019.
+//  Copyright © 2019 Mac14. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
